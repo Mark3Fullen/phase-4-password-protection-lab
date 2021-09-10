536476f655c467f1465c7fb61b64880c648cdad3
Mark <mark3fullen@gmail.com>
2021-09-09 23:00:52 -0400

    commit tree 0ae99de19581696783d83cdccb722c6940d40659

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
